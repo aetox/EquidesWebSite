e33764d831661242b5da5ed7ad8fe1187ede0ba4
Dimitri Kapolas <dimitri.kapolas04@gmail.com>
2023-02-21 23:51:41 +0100

    Ajout page connexion

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php include("header.php"); ?>
 
+<div class="accueil">
 
-<?php echo ("Accueil"); ?>
+    <img src="#" alt="Logo">
+
+        <form name="accueil_connexion" id="accueil_connexion">
+
+                <h1>Connexion</h1>
+
+            <input type="mail" name="mail" id="mail_connexion" placeholder="Mail" required>
+            <input type="password" name="password" id="password_connexion"  placeholder="Mot de passe"required>
+            <div>
+                <input type="checkbox" name="remember_me" id="remember_me" >
+                <label for="remember_me">Se souvenir de moi</label>
+            </div>
+            <button type="submit" >Se Connecter</button>
+            <a href="#">Mot de passe oublié ?</a>
+
+        </form>
+
+    <p>Pas de compte ? <a href="#">Créez-en un ici !</a></p> 
+
+</div>
+
+
+
+
 
 
 <?php include("footer.php"); ?>

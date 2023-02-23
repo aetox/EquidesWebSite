c1a0d03e2b40dc73dc3f6662558cb1d912e7bc69
andrmour <noe.andre57000@gmail.com>
2023-02-23 22:57:57 +0100

    Ajout page inscription

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
 
         <form name="accueil_connexion" id="accueil_connexion">
 
-                <h1>Connexion</h1>
+            <h1>Connexion</h1>
 
             <input type="mail" name="mail" id="mail_connexion" placeholder="Mail" required>
             <input type="password" name="password" id="password_connexion"  placeholder="Mot de passe"required>
             <div>
-                <input type="checkbox" name="remember_me" id="remember_me" >
+                <input type="checkbox" name="remember_me" id="remember_me" required>
                 <label for="remember_me">Se souvenir de moi</label>
             </div>
             <button type="submit" >Se Connecter</button>
@@ -20,7 +20,7 @@
 
         </form>
 
-    <p>Pas de compte ? <a href="#">Créez-en un ici !</a></p> 
+    <p>Pas de compte ? <a href="inscription.php">Créez-en un ici !</a></p> 
 
 </div>
 

d4857aec2fc888599dd5ae3f1f0ebcb8f884fef7
Dimitri Kapolas <dimitri.kapolas04@gmail.com>
2023-02-23 21:40:28 +0100

    Boostrap

--- header.php
+++ header.php
@@ -7,16 +7,37 @@
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="CSS/header.css">
     <link rel="stylesheet" href="CSS/footer.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Equides</title>
 </head>
 <body>
    
     <header>
-        <img src="ASSETS/logo_equides.png" alt="Logo">
+        <!-- <img src="ASSETS/logo_equides.png" alt="Logo">
         <ul>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="equides.php">Equides</a></li>
             <li><a href="ecurie.php">Ecurie</a></li>
             <li><a href="profil.php">Mon profil</a></li>
-        </ul>
-    </header>
+        </ul> -->
+        </header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">
+                    <img src="ASSETS/logo_equides.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-right">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav text-right">
+                    <a class="nav-link text-uppercase active color-white" aria-current="page" href="index.php">Accueil</a>
+                    <a class="nav-link text-uppercase" href="equides.php">Equides</a>
+                    <a class="nav-link text-uppercase" href="ecurie.php">Ecurie</a>
+                    <a class="nav-link text-uppercase" href="profil.php">Mon profil</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+  
+    

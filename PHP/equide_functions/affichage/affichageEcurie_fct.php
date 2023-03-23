67dfb4763ddc500a3761437965a6f4a7345d6b4a
aetox <core.editor>
2023-03-23 21:27:18 +0100

    maj

--- PHP/equide_functions/affichage/affichageEcurie_fct.php
+++ PHP/equide_functions/affichage/affichageEcurie_fct.php
@@ -11,13 +11,17 @@ $type_profil = $_SESSION['type_profil'];
 
 if($_SESSION['type_profil'] == "detenteur") {
 
-    $sql = "SELECT * FROM ` WHERE detenteur='$id_login'";
+    $sql = "SELECT * FROM `registre_equide` WHERE id_detenteur='$id_login'";
     $result = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+
+
+    
 
 }elseif($_SESSION['type_profil'] == "proprietaire"){
 
-    $sql = "SELECT * FROM lieudetention` WHERE proprietaire='$id_login'";
+    $sql = "SELECT * FROM `registre_equide` WHERE proprietaire='$id_login'";
     $result = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
 
 }else {?>
-        <h3><?=("Vous n'avez pas d'écurie");}?></h3> */
+        
+<h3><?=("Vous n'avez pas d'écurie");}?></h3>

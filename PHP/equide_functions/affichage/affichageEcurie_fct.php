aba5be2d1738c8329125306b11f618a0b75e5ed0
kXuT <benji.54000@live.fr>
2023-03-23 23:00:03 +0100

    sqlfonctionnel

--- PHP/equide_functions/affichage/affichageEcurie_fct.php
+++ PHP/equide_functions/affichage/affichageEcurie_fct.php
@@ -7,26 +7,28 @@ $typeProfil = $_SESSION['type_profil'];
 if(isset($_SESSION['id_detenteur'])){
 
     $idDetenteur = $_SESSION['id_detenteur'];
-    $sql = "SELECT * FROM `registre_equide` WHERE id_detenteur='$idDetenteur'";
+    $sql = "SELECT * FROM `registre_equide` JOIN `en_pension` ON registre_equide.id_registre = en_pension.id_registre JOIN `equide` ON equide.id_equide = en_pension.id_equide WHERE id_detenteur='$idDetenteur'";
     $result = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
 
     if (mysqli_num_rows($result) > 0) {      
 
-        while($equides = mysqli_fetch_array($result)){
+        while($rowData = mysqli_fetch_array($result)){
     
-            $SIRE = $equides['SIRE'];
-            $lienPdp = AffichagePhoto($mysqli,$SIRE);
+            $idRegistre = $rowData['id_registre'];
+            $idEquide = $rowData['id_equide'];
+
+            // $lienPdp = AffichagePhoto($mysqli,$rue);
           
         ?>
                 <div class="equide_bootstrap card " >
-                    <img src="../ASSETS/img_bdd/<?php echo $lienPdp?>" class="card-img-top" alt="Equidé n°<?php echo $SIRE?>">
+                    <img src="../ASSETS/img_bdd/" class="card-img-top" alt="Ecurie n°<?php echo $idRegistre?>">
                     <div class="card-body ">
-                        <h5 class="card-title"><strong><?php echo $equides['nom_equide'] ?></strong></h5>
+                        <h5 class="card-title"><strong><?php echo $idEquide ?></strong></h5>
                             <ul class="list-group list-group-flush">
-                                <li class="list-group-item">Sire : <?php echo $equides['SIRE'] ?></li>
-                                <li class="list-group-item">UELN : <?php echo $equides['UELN'] ?></li>
+                                <li class="list-group-item">Sire : <?php echo $rowData['SIRE'] ?></li>
+                                <li class="list-group-item">UELN : <?php echo $rowData['UELN'] ?></li>
                                 <li class="modification list-group-item"><a href="#">PDF - Carnet de Santé</a></li>
-                                <li class="modification list-group-item" id="affichageEquides_info"><a  href="equide_description.php?SIRE=<?php echo $equides['SIRE'];?>">plus d'info</a></li>
+                                <li class="modification list-group-item" id="affichageEquides_info"><a  href="equide_description.php?SIRE=<?php echo $rowData['SIRE'];?>">plus d'info</a></li>
                             </ul> 
                     </div>
                 </div>

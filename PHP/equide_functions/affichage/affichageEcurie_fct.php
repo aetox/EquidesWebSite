d624da334d32f78e637072bd5e9433a485d4cc81
kXuT <benji.54000@live.fr>
2023-03-23 22:20:22 +0100

    Commencement

--- PHP/equide_functions/affichage/affichageEcurie_fct.php
+++ PHP/equide_functions/affichage/affichageEcurie_fct.php
@@ -1,23 +1,64 @@
 <?php 
 
 $info_error = array();
+$idLogin = $_SESSION['id_login'];
+$typeProfil = $_SESSION['type_profil'];
 
-// Affichage taches en cours :
+if(isset($_SESSION['id_detenteur'])){
 
-$id_login = $_SESSION['id_login'];
-$type_profil = $_SESSION['type_profil'];
-
-
-
-if($_SESSION['type_profil'] == "detenteur") {
-
-    $sql = "SELECT * FROM ` WHERE detenteur='$id_login'";
+    $idDetenteur = $_SESSION['id_detenteur']
+    $sql = "SELECT * FROM `registre_equide` WHERE id_detenteur="$idDetenteur" FULL JOIN `en_pension` registre_equide.id_registre = en_pension.id_registre ";
     $result = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
 
-}elseif($_SESSION['type_profil'] == "proprietaire"){
+    if (mysqli_num_rows($result) > 0) {      
 
-    $sql = "SELECT * FROM lieudetention` WHERE proprietaire='$id_login'";
+        while($equides = mysqli_fetch_array($result)){
+    
+            $SIRE = $equides['SIRE'];
+            $lienPdp = AffichagePhoto($mysqli,$SIRE);
+          
+        ?>
+                <div class="equide_bootstrap card " >
+                    <img src="../ASSETS/img_bdd/<?php echo $lienPdp?>" class="card-img-top" alt="Equidé n°<?php echo $SIRE?>">
+                    <div class="card-body ">
+                        <h5 class="card-title"><strong><?php echo $equides['nom_equide'] ?></strong></h5>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Sire : <?php echo $equides['SIRE'] ?></li>
+                                <li class="list-group-item">UELN : <?php echo $equides['UELN'] ?></li>
+                                <li class="modification list-group-item"><a href="#">PDF - Carnet de Santé</a></li>
+                                <li class="modification list-group-item" id="affichageEquides_info"><a  href="equide_description.php?SIRE=<?php echo $equides['SIRE'];?>">plus d'info</a></li>
+                            </ul> 
+                    </div>
+                </div>
+    <?php }
+
+}elseif(isset($_SESSION['id_proprietaire']){
+
+    $idProprietaire = $_SESSION['id_proprietaire']
+    $sql = "SELECT * FROM `registre_equide` WHERE proprietaire='$id_login'";
     $result = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+    
+    if (mysqli_num_rows($result) > 0) {      
+
+        while($equides = mysqli_fetch_array($result)){
+    
+            $numSIRE = $equides['numSIRE'];
+            $lienPdp = AffichagePhoto($mysqli,$numSIRE);
+          
+        ?>
+                <div class="equide_bootstrap card " >
+                    <img src="../ASSETS/img_bdd/<?php echo $lienPdp?>" class="card-img-top" alt="Equidé n°<?php echo $numSIRE?>">
+                    <div class="card-body ">
+                        <h5 class="card-title"><strong><?php echo $equides['nom_equide'] ?></strong></h5>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Sire : <?php echo $equides['numSIRE'] ?></li>
+                                <li class="list-group-item">UELN : <?php echo $equides['numUELN'] ?></li>
+                                <li class="modification list-group-item"><a href="#">PDF - Carnet de Santé</a></li>
+                                <li class="modification list-group-item" id="affichageEquides_info"><a  href="equide_description.php?numSIRE=<?php echo $equides['numSIRE'];?>">plus d'info</a></li>
+                            </ul> 
+                    </div>
+                </div>
+    <?php }
 
 }else {?>
-        <h3><?=("Vous n'avez pas d'écurie");}?></h3> */
+        <h3><?=("Vous n'avez pas d'écurie");}?></h3>

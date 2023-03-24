619d372a36b313a9be003a109cbc2fe0ba63b882
aetox <core.editor>
2023-03-24 18:59:41 +0100

    form inscription en fonction du profil

--- JS/function.js
+++ JS/function.js
@@ -1,6 +1,35 @@
+//**************************** */
+//Modification du formaulraire en fonction du type de profil
 
 
+var click = document.getElementById('radio_type_profil').addEventListener('click',function test() {
 
+    const radios = document.getElementsByName('type_profil');
 
+    for (var i = 0; i <  radios.length; i++) {
+    if (radios[i].checked) {
 
+            switch(radios[i].value){
+                case "detenteur":
+                    var formDetenteur = document.getElementById('id_inscription_detenteur');
+                    formDetenteur.classList.add("display");
+                    var formProprietaire = document.getElementById('id_inscription_propietaire');
+                    formProprietaire.classList.remove("display")
+                    console.log("c'est un detenteur");
+                    break;
+                case "proprietaire":
+                    var formProprietaire = document.getElementById('id_inscription_propietaire');
+                    formProprietaire.classList.add("display");
+                    var formProprietaire = document.getElementById('id_inscription_detenteur');
+                    formProprietaire.classList.remove("display");
+                    console.log("c'est un proprio");
+                    break;
+                default:
+                    console.log("Rien");
+            }
+
+        }
+    }
+    
+})
 

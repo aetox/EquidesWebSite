bef3a34ca8bfe4cbee807ff8e69b74ea6fd25703
kXuT <benji.54000@live.fr>
2023-03-28 17:14:23 +0200

    petitchangement

--- PHP/equide_functions/affichage/affichageEcurie_fct.php
+++ PHP/equide_functions/affichage/affichageEcurie_fct.php
@@ -28,6 +28,7 @@ if(isset($_SESSION['id_detenteur'])){
     JOIN `groupement_veterinaire`
     ON veterinaire.id_groupement_veterinaire = groupement_veterinaire.id_groupement_veterinaire
     WHERE registre_equide.id_detenteur='$idDetenteur'";
+
     $results = mysqli_query($mysqli,$ecuriesql) or die(mysqli_error($mysqli));
     
     if (mysqli_num_rows($results) > 0) {
@@ -70,7 +71,8 @@ if(isset($_SESSION['id_detenteur'])){
             </ul>
     <?php }
 
-    $sql = "SELECT equide.nom, equide.id_equide
+    $sql =
+    "SELECT equide.nom, equide.id_equide
     FROM `registre_equide`
     JOIN `en_pension` ON registre_equide.id_registre = en_pension.id_registre
     JOIN `equide` ON equide.id_equide = en_pension.id_equide
@@ -84,7 +86,6 @@ if(isset($_SESSION['id_detenteur'])){
             $idEquide = $rowData['id_equide'];
             $nom = $rowData['nom'];
             // $lienPdp = AffichagePhoto($mysqli,$photo);
-          
     ?>
             <div class="equide_bootstrap card " >
                 <img src="../ASSETS/img_bdd/" class="card-img-top" alt="Nom du cheval : <?php echo $nom?>">
@@ -97,12 +98,11 @@ if(isset($_SESSION['id_detenteur'])){
                                     <li class="modification list-group-item"><a href="#">PDF - Carnet de Santé</a></li>
                                     <li class="modification list-group-item"><a href="#">PDF - Fiche de Transport</a></li>
                                     <li class="modification list-group-item" id="affichageEquides_info"><a  href="equide_description.php?SIRE=<?php echo $rue;?>">plus d'info</a></li>
-
                         </ul> 
                 </div>
             </div>
-    <?php }}
-    else{ ?>
+    <?php
+    }}else{ ?>
         <h3>Vous n'avez pas d'équidés.</h3>
 
 <?php   }}
@@ -110,30 +110,25 @@ if(isset($_SESSION['id_detenteur'])){
 elseif(isset($_SESSION['id_proprietaire'])) {
 
     $idProprietaire = $_SESSION['id_proprietaire'];
-    $sql = "SELECT * FROM `registre_equide` WHERE proprietaire='$id_login'";
-    $result = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
-    
-    if (mysqli_num_rows($result) > 0) {      
+    $sqlproprio =
+    "SELECT *
+    FROM `registre_equide`
+    WHERE proprietaire='$id_login'";
 
-        while($equides = mysqli_fetch_array($result)){
+    $resultproprio = mysqli_query($mysqli,$sqlproprio) or die(mysqli_error($mysqli));
+    
+    if (mysqli_num_rows($resultproprio) > 0) {      
+
+        while($equides = mysqli_fetch_array($resultproprio)){
     
             $numSIRE = $equides['numSIRE'];
-            $lienPdp = AffichagePhoto($mysqli,$numSIRE);
-          
-            ?>
+            $lienPdp = AffichagePhoto($mysqli,$numSIRE);?>
+            
             <div class="equide_bootstrap card " >
                 <img src="../ASSETS/img_bdd/" class="card-img-top" alt="Ecurie n°<?php echo $idRegistre?>">
                 <div class="card-body ">
                     <h5 class="card-title"><strong><?php echo $idEquide ?></strong></h5>
                         <ul class="list-group list-group-flush">
-                            <?php
-                                $sql2 = "SELECT * FROM `registre_equide` JOIN `en_pension` ON registre_equide.id_registre = en_pension.id_registre JOIN `equide` ON equide.id_equide = en_pension.id_equide WHERE id_detenteur='$idDetenteur'";
-                                $result2 = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
-                                if (mysqli_num_rows($result2) > 0) {
-                        
-                                }else{?>
-                                    <li class="list-group-item">Vous n'avez pas d'équidés</li>
-                                <?php }?>
                             <li class="list-group-item">Sire : <?php echo $rowData['SIRE'] ?></li>
                             <li class="list-group-item">UELN : <?php echo $rowData['UELN'] ?></li>
                             <li class="modification list-group-item"><a href="#">PDF - Carnet de Santé</a></li>
@@ -141,7 +136,7 @@ elseif(isset($_SESSION['id_proprietaire'])) {
                         </ul> 
                 </div>
             </div>
-<?php }}}
-
-else {?>
-        <h3>"Vous n'avez pas d'écurie"</h3><?php }?>
+<?php
+}}}else {
+    header("Location: index.php");
+}ob_end_flush(); ?>

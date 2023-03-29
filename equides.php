7dd134473a81e65cf13ba0f4e65839e9724bc1a5
andrmour <noe.andre57000@gmail.com>
2023-03-29 15:04:00 +0200

    Formulaire équidé 28/03/2023

--- equides.php
+++ equides.php
@@ -2,28 +2,45 @@
 $titre ="Mes équides";
 ob_start();
 include_once("header.php");
+	
 if(isset($_SESSION['logged_user'])) {
+	if(isset($_SESSION['id_detenteur'])){
 ?>
-<?php include_once('PHP/equide_functions/modification/ajoutEquides_fct.php') ?>
+	<?php include_once('PHP/equide_functions/modification/ajoutEquides_fct.php') ?>
 
-<div class="equides">
+	<div class="equides">
+
+		<h1 class="titre_1">Mes équidés</h1>
+
+		<!-- Ajouter la bouton recherche -->
+
+		<a href="ajout_equides.php" class="boutton_vertV2"><img src="ASSET/ico/plus2.png">équidé</a>
+
+					
+		<div id="Equides">
+			<!-- faire un appel de la fonction affichage erreurs. -->
+			<?php include_once('PHP/equide_functions/affichage/affichageEquides_fct.php') ?>
+		</div>
+		
+		
+	</div>
+
+<?php }elseif(isset($_SESSION['id_proprietaire'])){
+?>
+	<div class="equides">
 
 	<h1 class="titre_1">Mes équidés</h1>
 
-	<!-- Ajouter la bouton recherche -->
-
-	<a href="ajout_equides.php" class="boutton_vertV2"><img src="ASSET/ico/plus2.png">équidé</a>
-
-				
 	<div id="Equides">
-		<!-- faire un appel de la fonction affichage erreurs. -->
-		<?php include_once('PHP/equide_functions/affichage/affichageEquides_fct.php') ?>
+			<!-- faire un appel de la fonction affichage erreurs. -->
+			<?php include_once('PHP/equide_functions/affichage/affichageEquides_fct.php') ?>
 	</div>
-	
-	
-</div>
 
-<?php include_once("footer.php"); ?>
+	</div>
+
+<?php };
+	
+include_once("footer.php"); ?>
 <?php }else {
     header("Location: index.php");
 }ob_end_flush(); ?>

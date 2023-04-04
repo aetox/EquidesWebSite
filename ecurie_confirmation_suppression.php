9f38ce6611954823747330b24c1eb7aac82ffbd2
kXuT <benji.54000@live.fr>
2023-04-04 08:39:45 +0200

    ajoutmodifsuppecurie

--- ecurie_confirmation_suppression.php
+++ ecurie_confirmation_suppression.php
@@ -5,9 +5,19 @@ include_once("header.php");
 
 if(isset($_SESSION['logged_user']) && isset($_SESSION['id_detenteur'])) {
     $idDetenteur = $_GET['id_detenteur'];
+    $sql =
+    "SELECT id_detenteur
+    FROM registre_equide
+    WHERE id_detenteur = $idDetenteur";
+    $result = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+    if (mysqli_num_rows($result) > 0) {
 ?>
     <li class="modification list-group-item" id="affichageEquides_info"><a href="PHP/equide_functions/modification/suppressionEcurie.php?id_detenteur=<?= $idDetenteur;?>">Suppression</a></li>
 <?php
+    }
+    else {
+        header("Location: index.php");
+    }
 }
 else {
     header("Location: index.php");

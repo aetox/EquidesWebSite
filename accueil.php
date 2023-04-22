8e2b659942de36167daf7662a0d5b4eb39a46d2d
andrmour <noe.andre57000@gmail.com>
2023-04-22 16:03:34 +0200

    Refonte landing page + ico logo

--- accueil.php
+++ accueil.php
@@ -7,7 +7,12 @@ if(isset($_SESSION['logged_user'])) {
 
 <div class="accueil">
 
-    <h1 class="accueil_titre">Bienvenue <?= $_SESSION['prenom'];?>,</h1>
+<div class="left">
+    <img src="ASSETS\ico\horse(1).png" alt="" width="500px">
+</div>
+
+<div class="right">
+    <h1 class="accueil_titre">Bienvenue <br><?= $_SESSION['prenom'];?>,</h1>
 
 
     <h3 class="accueil_soustitre">Suivre vos chevaux n'a jamais été aussi simple.<br>
@@ -16,10 +21,12 @@ if(isset($_SESSION['logged_user'])) {
     <br></h3>
 
     <div class="accueil_div_bouttons">
-        <a href="equides.php" class="boutton_orange"><img src="ASSETS\ico\ico-fer-a-cheval.png">Mes équidés</a>
-        <a href="ecurie.php" class="boutton_orange"><img src="ASSETS\ico\ico-obstacle.png">Mon écurie</a>
+        <a href="equides.php" class="boutton_orange_accueil"><img src="ASSETS\ico\ico-fer-a-cheval.png">Mes équidés</a>
+        <a href="ecurie.php" class="boutton_orange_accueil"><img src="ASSETS\ico\ico-obstacle.png">Mon écurie</a>
         <a href="carnet_transport.php" class="boutton_vert"><img src="ASSETS\ico\ico-panneau.png">Je voyage</a>         
     </div>
+</div>
+    
 
 </div>
 
